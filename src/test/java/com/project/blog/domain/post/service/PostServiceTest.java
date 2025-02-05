604181b6b4906702d994636376374755166b751d
김명호 <audgh919@gmail.com>
2025-02-05 16:19:24 +0900

    Feat: 포스트서비스 테스트코드 추가

---
+++ src/test/java/com/project/blog/domain/post/service/PostServiceTest.java
@@ -0,0 +1,174 @@
+package com.project.blog.domain.post.service;
+
+import com.project.blog.domain.comment.entity.Comment;
+import com.project.blog.domain.comment.repository.CommentRepository;
+import com.project.blog.domain.post.dto.response.PostCommentsResponseDto;
+import com.project.blog.domain.post.dto.response.PostLikesUserResponseDto;
+import com.project.blog.domain.post.dto.response.PostResponseDto;
+import com.project.blog.domain.post.entity.Post;
+import com.project.blog.domain.post.repository.PostRepository;
+import com.project.blog.domain.postlike.repository.PostLikeRepository;
+import com.project.blog.domain.user.entity.User;
+import com.project.blog.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class PostServiceTest {
+
+    private Pageable pageable;
+
+    @Mock
+    private PostRepository postRepository;
+    @Mock
+    private UserRepository userRepository;
+    @Mock
+    private CommentRepository commentRepository;
+    @Mock
+    private PostLikeRepository postLikeRepository;
+
+    @InjectMocks
+    private PostService postService;
+
+    @BeforeEach
+    void setup() {
+        this.pageable = PageRequest.of(0, 10);
+    }
+
+    @Test
+    @DisplayName("하나의 포스팅 조회")
+    void findPostTest() {
+        // given
+        Long postId = 1L;
+
+        Post post = new Post("제목1", "내용1");
+        post.setUser(new User());
+
+        given(postRepository.findByPostWithUserOrElseThrow(postId)).willReturn(post);
+        given(postLikeRepository.sizeOfPost(postId)).willReturn(10L);
+
+        // when
+        PostResponseDto result = postService.findPost(postId);
+
+        // then
+        assertThat(result.getContent()).isEqualTo("내용1");
+        assertThat(result.getTitle()).isEqualTo("제목1");
+        assertThat(result.getLikes()).isEqualTo(10L);
+
+        verify(postRepository).findByPostWithUserOrElseThrow(postId);
+        verify(postLikeRepository).sizeOfPost(postId);
+    }
+
+    @Test
+    @DisplayName("모든 포스팅 조회")
+    void findAllPostsTest() {
+        // given
+        Post post1 = new Post("제목1", "내용1");
+        Post post2 = new Post("제목2", "내용2");
+        Post post3 = new Post("제목3", "내용3");
+
+        post1.setUser(new User(1L));
+        post2.setUser(new User(2L));
+        post3.setUser(new User(3L));
+
+        List<Post> postList = List.of(post1, post2, post3);
+
+        Page<Post> postPage = new PageImpl<>(postList);
+
+        given(postRepository.findAllPosts(pageable)).willReturn(postPage);
+
+        // when
+        Page<PostResponseDto> result = postService.findAllPosts(pageable);
+
+        // then
+        assertThat(result.getTotalElements()).isEqualTo(3);
+        assertThat(result.getContent().get(0).getTitle()).isEqualTo("제목1");
+        assertThat(result.getContent().get(1).getContent()).isEqualTo("내용2");
+        assertThat(result.getContent().get(2).getTitle()).isEqualTo("제목3");
+
+        verify(postRepository).findAllPosts(pageable);
+    }
+
+    @Test
+    @DisplayName("한 포스팅의 댓글 전체조회")
+    void findAllCommentsOfPostTest() {
+        // given
+        Long postId = 1L;
+        given(postRepository.existsById(postId)).willReturn(true);
+
+        Comment comment1 = new Comment("댓글1");
+        comment1.setUser(new User());
+
+        Comment comment2 = new Comment("댓글2");
+        comment2.setUser(new User());
+
+        Comment comment3 = new Comment("댓글3");
+        comment3.setUser(new User());
+
+        List<Comment> commentList = List.of(comment1, comment2, comment3);
+
+        Page<Comment> commentPage = new PageImpl<>(commentList);
+
+        given(commentRepository.findAllCommentsWithPost(postId, pageable)).willReturn(commentPage);
+
+        // when
+        Page<PostCommentsResponseDto> result = postService.findAllCommentsOfPost(postId, pageable);
+
+        // then
+        assertThat(result.getTotalElements()).isEqualTo(3);
+        assertThat(result.getContent().get(0).getComment()).isEqualTo("댓글1");
+        assertThat(result.getContent().get(1).getComment()).isEqualTo("댓글2");
+        assertThat(result.getContent().get(2).getComment()).isEqualTo("댓글3");
+
+        verify(postRepository).existsById(postId);
+        verify(commentRepository).findAllCommentsWithPost(postId, pageable);
+    }
+
+    @Test
+    @DisplayName("한 포스팅의 좋아요 누른 유저의 정보 조회")
+    void findAllLikesUserDataTest() {
+        // given
+        Long postId = 1L;
+        given(postRepository.existsById(postId)).willReturn(true);
+
+        User user1 = new User("닉네임1");
+        User user2 = new User("닉네임2");
+        User user3 = new User("닉네임3");
+        User user4 = new User("닉네임4");
+
+        List<User> userList = List.of(user1, user2, user3, user4);
+        Page<User> userPage = new PageImpl<>(userList);
+
+        given(postLikeRepository.findPostLikesByUserData(postId, pageable)).willReturn(userPage);
+
+        // when
+        Page<PostLikesUserResponseDto> result = postService.findAllLikesUserData(postId, pageable);
+
+        // then
+        assertThat(result.getTotalElements()).isEqualTo(4L);
+        assertThat(result.getContent().get(0).getUserNickname()).isEqualTo("닉네임1");
+        assertThat(result.getContent().get(2).getUserNickname()).isEqualTo("닉네임3");
+
+        verify(postRepository).existsById(postId);
+        verify(postLikeRepository).findPostLikesByUserData(postId, pageable);
+    }
+
+
+
+}
